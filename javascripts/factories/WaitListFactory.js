33dde71270e22958c9f82664c72790c455244c71
lsimmons1832 <lsimmons1832@gmail.com>
2017-06-20 15:50:56 -0500

    add function to check for waiting list

--- javascripts/factories/WaitListFactory.js
+++ javascripts/factories/WaitListFactory.js
@@ -1,7 +1,6 @@
 app.factory("WaitListFactory", function($http, $q, FIREBASE_CONFIG) {
 	
 	let addMeToTheList = (list) => {
-		console.log("what am I sending for the wait list?", list);
 		return $q((resolve, reject) =>{
 			$http.post(`${FIREBASE_CONFIG.databaseURL}/waitinglists.json`, 
 				JSON.stringify({
@@ -17,5 +16,24 @@ app.factory("WaitListFactory", function($http, $q, FIREBASE_CONFIG) {
 		});
 	};
 
-	return{addMeToTheList: addMeToTheList};
+	let getList = (isbn) => {
+		let nextUp = [];
+		return $q((resolve, reject) =>{
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/waitinglists.json?orderBy="isbn"&equalTo="${isbn}"`)
+			.then((waitList) =>{
+			let lisitng = waitList.data;
+			if(lisiting !== null){
+				Object.keys(lisiting).forEach((key) =>{
+				listing[key].id = key;
+				nextUp.push(listing[key]);
+				});
+			}
+			resolve(nextUp);
+		}).catch((error) => {
+			reject(error);
+		});
+	  });
+	};
+
+	return{addMeToTheList: addMeToTheList, getList: getList};
 });
